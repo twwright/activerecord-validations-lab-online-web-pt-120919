f57a2ad3f2f576072eff4980734262cac82be0ac
Thomas Wright <twwright@gmail.com>
2020-05-10 21:57:47 +0000

    Done.

---
+++ app/validators/clickbait_validator.rb
@@ -0,0 +1,16 @@
+class ClickbaitValidator < ActiveModel::Validator
+  
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def validate(record)
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match(record.title) }
+      record.errors[:title] << "Title must be clickbait"
+    end
+  end
+
+end
